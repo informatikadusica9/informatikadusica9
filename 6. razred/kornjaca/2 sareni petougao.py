72d0f0637fa0bd366ac25f57a3cf0befcdaa9bde
informatikadusica9 <166441474+informatikadusica9@users.noreply.github.com>
2024-04-21 21:54:50 +0200

    Add files via upload

---
+++ 6. razred/kornjaca/2 sareni petougao.py
@@ -0,0 +1,24 @@
+from turtle import*
+shape("turtle")
+
+width(4)
+color("red")
+fd(100); rt(360/5)
+
+width(10)
+color("green")
+fd(100); rt(360/5)
+
+width(3)
+color("blue")
+fd(100); rt(360/5)
+
+width(14)
+color("yellow")
+fd(100); rt(360/5)
+
+width(20)
+color("purple")
+fd(100); rt(360/5)
+
+
