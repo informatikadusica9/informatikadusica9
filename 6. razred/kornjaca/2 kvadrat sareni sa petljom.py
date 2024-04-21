4042bd1e62e1e8a8fa55c278e3027f0d1a53a6e1
informatikadusica9 <166441474+informatikadusica9@users.noreply.github.com>
2024-04-21 21:55:51 +0200

    Add files via upload

---
+++ 6. razred/kornjaca/2 kvadrat sareni sa petljom.py
@@ -0,0 +1,9 @@
+from turtle import*
+width(10)
+color("violet")
+boje=["red", "green", "blue", "yellow"]
+for i in range(4):
+    color(boje[i])
+    fd(100);rt(90)
+    
+      
---
+++ 6. razred/kornjaca/4 2 trougla for petlja.py
@@ -0,0 +1,12 @@
+from turtle import*
+shape("turtle")
+width(4)
+color("blue")
+for i in range (3):
+    fd(200);rt(120)
+fd(100);rt(120)
+for i in range (3):
+    fd(100);rt(-120)
+
+
+
